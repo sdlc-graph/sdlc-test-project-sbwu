cf8bd857a9d150391d545462bf35761cd7f57251
Steven Wu <sbwu@google.com>
2026-02-10 20:54:38 +0000

    foo change

--- app/app.py
+++ app/app.py
@@ -6,5 +6,5 @@ PORT = 8000  # You can change the port number here
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"serving at port {PORT}")
+    print(f"serving at porty {PORT}")
     httpd.serve_forever()
